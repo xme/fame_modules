9db28018f8f4cd18a1c0f6025273026fe97f7a79
Xavier Mertens <xavier@rootshell.be>
2020-05-18 15:37:13 +0200

    Multiple changes/fix

--- processing/floss_str/floss_str.py
+++ processing/floss_str/floss_str.py
@@ -1,11 +1,20 @@
 import os
 import re
 
-import vivisect
-from floss import identification_manager as id_man
-from floss import main
-from floss import stackstrings
-from floss import strings as static
+try:
+    import vivisect
+    HAVE_VIVISECT = True
+except ImportError:
+    HAVE_VIVISECT = False
+
+try:
+    from floss import identification_manager as id_man
+    from floss import main
+    from floss import stackstrings
+    from floss import strings as static
+    HAVE_FLOSS = True
+except ImportError:
+    HAVE_FLOSS = False
 
 from fame.core.module import ProcessingModule
 from fame.common.utils import tempdir
@@ -34,8 +43,24 @@ class floss_str(ProcessingModule):
             "type": "integer",
             "default": 5000,
             "description": "Maximum number of strings to report."
+        },
+        {
+            'name': 'interesting_strings_file',
+            'type': 'str',
+            'description': 'File containing interesting/suspicious strings, you should specify the full path.'
+        },
+        {
+            'name': 'ignored_strings_file',
+            'type': 'str',
+            'description': 'File containing strings to ignore, you should specify the full path.'
         }
     ]
+
+    def initialize(self):
+        if not HAVE_VIVISECT:
+            raise ModuleInitializationError(self, "Missing dependency: vivisect")
+        if not HAVE_FLOSS:
+            raise ModuleInitializationError(self, "Missing dependency: floss")
 
     def each(self, target):
         self.results = {
@@ -77,12 +102,32 @@ class floss_str(ProcessingModule):
                  vw, main.get_all_plugins(), selected_functions
             )
         except Exception as e:
-            self.error('error','Cannot analyze file {}'.format(target))
+            self.log('error','Cannot analyze file {}'.format(target))
             self.results = None
             return False
 
+        # Load list of IOC's
         try:
-            # Decode & extract hidden & encoded strings
+            with open(self.interesting_strings_file) as f:
+               interesting_strings = f.read().splitlines()
+            self.log('info', 'Loaded interesting strings from {}'.format(self.interesting_strings_file))
+        except:
+            # No IOC file, create an empty list           
+            self.log('info', 'No file with interesting strings defined')
+            interesting_strings = []
+
+        # Load list of ignored strings
+        try:
+            with open(self.ignored_strings_file) as f:
+               ignored_strings = f.read().splitlines()
+            self.log('info', 'Loaded ignored strings from {}'.format(self.ignored_strings_file))
+        except:
+            # No IOC file, create an empty list           
+            self.log('info', 'No file with ignored strings defined')
+            ignored_strings = []
+
+        # Decode & extract hidden & encoded strings
+        try:
             decoded_strings = main.decode_strings(
                 vw, decoding_functions_candidates, self.minimum_string_len
             )
@@ -99,19 +144,48 @@ class floss_str(ProcessingModule):
             self.results = None
             return False
 
+        # Populate results[] with found strings
         if len(decoded_strings) or len(stack_strings):
-            # convert Floss strings into regular, readable strings
             self.log('info', 'Found stack or decoded strings')
-            for idx, s in enumerate(decoded_strings):
-                decoded_strings[idx] = main.sanitize_string_for_printing(s.s)
-                self.results['decoded_strings'].append(str(main.sanitize_string_for_printing(s.s)))
+            for s in enumerate(decoded_strings):
+                buffer = main.sanitize_string_for_printing(s.s)
+                skip = False
+                for ignore in ignored_strings:
+                   if str(buffer).find(ignore) >= 0:
+                     skip = True
+                     break
+                if not skip:
+                    self.results['decoded_strings'].append(buffer)
+                    for ioc in interesting_strings:
+                        if str(buffer).find(ioc) >= 0:
+                           self.results['warnings'].append('Found suspicious string: {}'.format(buffer))
+                           break
 
-            for idx, s in enumerate(stack_strings):
-                stack_strings[idx] = s.s
-                self.results['stack_strings'].append(str(s.s))
+            for s in enumerate(stack_strings):
+                skip = False
+                for ignore in ignored_strings:
+                   if str(s.s).find(ignore) >= 0:
+                     skip = True
+                     break
+                if not skip:
+                    self.results['stack_strings'].append(s.s)
+                    for ioc in interesting_strings:
+                        if str(s.s).find(ioc) >= 0:
+                           self.results['warnings'].append('Found suspicious string: {}'.format(s.s))
+                           break
         else:
             self.log('info', 'Found static strings')
             for s in static_strings:
-                self.results['static_strings'].append(str(s))
+                skip = False
+                for ignore in ignored_strings:
+                    if str(s).find(ignore) >= 0:
+                        skip = True
+                        break
+                if not skip:
+                    self.results['static_strings'].append(s)
+                    for ioc in interesting_strings:
+                        if str(s).find(ioc) >= 0:
+                           self.results['warnings'].append('Found suspicious string: {}'.format(s))
+                           break
 
         return True
